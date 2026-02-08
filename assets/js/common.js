a852d3882882fa68cd83353cff4a994116552e25
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-02-08 12:07:44 +0300

    update year

---
+++ assets/js/common.js
@@ -0,0 +1,7 @@
+function updateCurrentYear() {
+    const currentYear = new Date().getFullYear();
+    const yearElement = document.getElementById('current-year');
+    if (yearElement) {
+        yearElement.textContent = currentYear;
+    }
+}
