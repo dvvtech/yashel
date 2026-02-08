b853794d755a441561258b9eaceb4f7733efb66c
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-02-08 12:20:39 +0300

    refactoring

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,15 @@
+
+async function loadComponent(id, file) {
+    const response = await fetch(file);
+    const content = await response.text();
+    document.getElementById(id).innerHTML = content;
+
+    // После загрузки футера обновляем год
+    if (id === "footer") {
+        updateCurrentYear();
+    }
+}
+
 function updateCurrentYear() {
     const currentYear = new Date().getFullYear();
     const yearElement = document.getElementById('current-year');
